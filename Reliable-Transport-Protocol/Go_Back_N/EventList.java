c51e13f1c98aea1a05253a9736b5e8ff44bf7d0d
Yigang Wang <ico.yigang@hotmail.com>
2015-12-06 17:25:19 -0500

    Create EventList.java

---
+++ Reliable-Transport-Protocol/Go_Back_N/EventList.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Yigang on 10/27/2015.
+ */
+public interface EventList
+{
+    public boolean add(Event e);
+    public Event removeNext();
+    public String toString();
+    public Event removeTimer(int entity);
+    public double getLastPacketTime(int entityTo);
+}
